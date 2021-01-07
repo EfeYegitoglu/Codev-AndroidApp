b5c6de607c6f60355107f18d533aa9461875aba1
Efe Yegitoğlu <76888997+EfeYegitoglu@users.noreply.github.com>
2021-01-07 23:11:46 +0300

    Update db_config.php

--- WebService/db_config.php
+++ WebService/db_config.php
@@ -1,6 +1,6 @@
 <?php
     define('DB_USER',"id14660659_efeyegitoglu");
-    define('DB_PASSWORD',"Kamikaze.1905");
-    define('DB_DATABASE',"id14660659_efe");
+    define('DB_PASSWORD',"");
+    define('DB_DATABASE',"");
     define('DB_SERVER',"localhost");
 ?>
